ae2050b497176c0b5239a9e21dfaa230e0dac5ab
iobio-zjut <43274315+iobio-zjut@users.noreply.github.com>
2026-01-25 11:38:59 +0800

    Update affine.py

--- utils/affine.py
+++ utils/affine.py
@@ -18,9 +18,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-############################
-# 仿射平移和旋转
-############################
+
 from __future__ import annotations
 from typing import Tuple, Any, Sequence, Callable
 
