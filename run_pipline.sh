e7ed9995a3dc6f58d3b7fea718614c464097ff5d
iobio-zjut <43274315+iobio-zjut@users.noreply.github.com>
2026-04-29 09:58:50 +0800

    update
    Removed the file preparation steps for the Ensemble server and esm extraction.

--- run_pipline.sh
+++ run_pipline.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-########## file_prepare#########
-cd "./Ensemble_server"
-
-mkdir "./6d7y_A/example"
-python ./lm_embedding_preparation.py --pdb_path ./example/6d7y_A/input/6d7y_A_0.pdb --out_file ./example/fasta/seq.fasta
-
-## get esm ##
-target_folder="./6d7y_A"
-base_folder=".."
-name=$(basename "$target_folder")
-mkdir -p "$target_folder/esm"
-
-python ./esm/scripts/extract.py \
-  esm2_t33_650M_UR50D \
-  "$target_folder/fasta/seq.fasta" \
-  "$target_folder/esm" \
-  --repr_layers 33 \
-  --include per_tok \
-  --truncation_seq_length 10000 \
-  --model_dir "$base_folder/esm_models" \
-  --class_name "$name"
-
 ###########DiffEnsemble##########
 target_folder="./example/6d7y_A"
 for i in {1..5}; do
